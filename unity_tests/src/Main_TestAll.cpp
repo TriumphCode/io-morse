454c1300f298fdaaacfcebbae75ccdf4d7308e57
lukaszsx <inzhtc@gmail.com>
2018-08-21 23:51:44 +0200

    Add new project with googletestframework support

---
+++ unity_tests/src/Main_TestAll.cpp
@@ -0,0 +1,17 @@
+/*
+ * Main_TestAll.cpp
+ *
+ *  Created on: Aug 21, 2018
+ *      Author: lukaszsx
+ */
+
+
+#include <stdio.h>
+#include "gtest/gtest.h"
+
+GTEST_API_ int main(int argc, char **argv) {
+  printf("Running main() from gtest_main.cc\n");
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
+
