58c9cc8840f4a71088bb590a3e24249144242337
lukaszsx <inzhtc@gmail.com>
2018-08-23 08:36:43 +0200

    Prepare small test case for morse encoder

--- unity_tests/tests/functional_test_suite/morse_input.cpp
+++ unity_tests/tests/functional_test_suite/morse_input.cpp
@@ -5,6 +5,14 @@
  *      Author: lukaszsx
  */
 
+#include "morse_encoder.c"
+#include "gtest/gtest.h"
+
+TEST(Morse_encoder_test, Correct_encoding)
+{
+	char * actual = morse('S');
+	ASSERT_STREQ(actual ,"...");
+}
 
 
 
