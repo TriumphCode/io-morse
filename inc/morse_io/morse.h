c70173de628f50f0e5e64da2080c5e47702d55c3
lukaszsx <inzhtc@gmail.com>
2018-08-23 08:50:20 +0200

    Reorganise morse_encoder to morse_io library.

---
+++ inc/morse_io/morse.h
@@ -0,0 +1,54 @@
+/*
+ * morse.h
+ *
+ *  Created on: Aug 23, 2018
+ *      Author: lukaszsx
+ */
+
+#ifndef MORSE_IO_MORSE_H_
+#define MORSE_IO_MORSE_H_
+
+static const char* CHAR_TO_MORSE[128] = {
+        NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+        NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+        NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+        NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+        NULL, "-.-.--", ".-..-.", NULL, NULL, NULL, NULL, ".----.",
+        "-.--.", "-.--.-", NULL, NULL, "--..--", "-....-", ".-.-.-", "-..-.",
+        "-----", ".----", "..---", "...--", "....-", ".....", "-....", "--...",
+        "---..", "----.", "---...", NULL, NULL, "-...-", NULL, "..--..",
+        ".--.-.", ".-", "-...", "-.-.", "-..", ".", "..-.", "--.",
+        "....", "..", ".---", "-.-", ".-..", "--", "-.", "---",
+        ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--",
+        "-..-", "-.--", "--..", NULL, NULL, NULL, NULL, "..--.-",
+        NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+        NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+        NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+        NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+};
+
+static const char* MORSE_TO_CHAR[128] = {
+        NULL, NULL, "E", "T", "I", "N", "A", "M",
+        "S", "D", "R", "G", "U", "K", "W", "O",
+        "H", "B", "L", "Z", "F", "C", "P", NULL,
+        "V", "X", NULL, "Q", NULL, "Y", "J", NULL,
+        "5", "6", NULL, "7", NULL, NULL, NULL, "8",
+        NULL, "/", NULL, NULL, NULL, "(", NULL, "9",
+        "4", "=", NULL, NULL, NULL, NULL, NULL, NULL,
+        "3", NULL, NULL, NULL, "2", NULL, "1", "0",
+        NULL, NULL, NULL, NULL, NULL, NULL, NULL, ":",
+        NULL, NULL, NULL, NULL, "?", NULL, NULL, NULL,
+        NULL, NULL, "\"", NULL, NULL, NULL, "@", NULL,
+        NULL, NULL, NULL, NULL, NULL, NULL, "'", NULL,
+        NULL, "-", NULL, NULL, NULL, NULL, NULL, NULL,
+        NULL, NULL, ".", NULL, "_", ")", NULL, NULL,
+        NULL, NULL, NULL, ",", NULL, "!", NULL, NULL,
+        NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL
+};
+
+const char* char_to_morse(char);
+const char* morse_to_char(const char*);
+int morse_to_index (const char*);
+
+
+#endif /* MORSE_IO_MORSE_H_ */
